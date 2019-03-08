48f27dea08f82a33dd5017daab8f8db9546c1015
BoClow <boclow34@gmail.com>
2019-03-08 16:49:38 -0600

    added apis

---
+++ api.js
@@ -0,0 +1,39 @@
+
+
+export function getDailyStore() {
+    return fetch('https://fortnite-public-api.theapinetwork.com/prod09/store/get')
+        .then((response) => {
+            return response.json()})
+        .catch((error) => {
+        console.error(error);
+        });
+}
+
+export function getUserID(username) {
+    return fetch('https://fortnite-public-api.theapinetwork.com/prod09/users/id?username='+username)
+        .then((response) => {
+            return response.json()})
+        .catch((error) => {
+            console.error(error);
+        });
+}
+
+
+export function getUserStats(id) {
+    return fetch('https://fortnite-public-api.theapinetwork.com/prod09/users/public/br_stats_v2?user_id='+id)
+        .then((response) => {
+            return response.json()})
+        .catch((error) => {
+        console.error(error);
+        });
+}
+
+export function getChallenges(season) {
+    return fetch('https://fortnite-public-api.theapinetwork.com/prod09/challenges/get?season='+season)
+        .then((response) => {
+            return response.json()})
+        .catch((error) => {
+        console.error(error);
+        });
+}
+
