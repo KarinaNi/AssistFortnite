d7cd296852ef7cd3a703088ead664c580ebbecba
BoClow <boclow34@gmail.com>
2019-03-11 13:01:57 -0500

    styles added

---
+++ styles.js
@@ -0,0 +1,18 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    statTrackerInputView: {
+        flexDirection: 'row',
+        padding: 25,
+        justifyContent:'center',
+        alignItems: 'center',
+    },
+    textInputView: {
+        padding:10,
+        borderWidth: 2,
+
+    }
+})
