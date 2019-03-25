bc08e882d1d30065dd5bf4c2fd29207c70383fed
Connor <connorb999@gmail.com>
2019-03-25 13:54:21 -0500

    Update WeeklyChallengesScreen.js

--- screens/WeeklyChallengesScreen.js
+++ screens/WeeklyChallengesScreen.js
@@ -1,14 +1,68 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text, TouchableOpacity, ImageBackground, ActivityIndicator, ListView, Alert} from 'react-native';
+import { getChallenges } from '../api';
+import styles from '../styles';
+import background from '../assets/images/background.jpg'
 
 export default class WeeklyChallengesScreen extends React.Component {
   static navigationOptions = {
     title: 'Weekly Challenges',
   };
 
+  constructor(props) {
+    super(props);  
+    let dataSource = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      weeks: dataSource.cloneWithRows([1,2,3,4,5,6,7,8,9,10]),
+      challenges: []
+    };
+    //get data using API
+    let challenges;
+    let season = "current";
+    getChallenges(season).then(challengesVal => {
+      if(challengesVal === undefined) {
+        Alert.alert('External Error',
+          'Data could not be found',
+          [
+            {text: 'OK', onPress: () => console.log('OK Pressed')},
+          ],
+          { cancelable: false })
+        return;
+      }      
+      this.state.challenges = challengesVal;
+    })
+  }
+
+  handleWeekButton(rowData){
+    console.log(" current week: " + this.state.challenges.currentweek);
+    console.log("week selected: " + rowData);
+    if(rowData > this.state.challenges.currentweek){
+      Alert.alert("Week " + rowData + " has not happened yet.")
+    }else{
+      this.props.navigation.navigate("WeekScreen", {week:rowData, challenges:this.state.challenges});
+    }
+  }
+
   render() {
     return (
-      <View></View>
+      <View style={styles.container}>
+        <ImageBackground source={background} style={{flex:1}}>
+          <ListView
+            dataSource={this.state.weeks}
+            renderRow={(rowData) => <Text
+              style={styles.weeksList}
+              onPress={this.handleWeekButton.bind(this, rowData)}>{'Week ' + rowData}
+              </Text>
+            }
+          />
+        </ImageBackground>
+        
+        {this.state.loading &&
+          <View style={styles.activityIndicatorStyle}>
+            <ActivityIndicator size='large' color='#152D53' />
+          </View>
+        }
+      </View>
     )
   }
 }
