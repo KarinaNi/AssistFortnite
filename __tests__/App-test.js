f7bd91d2ecc646dadcfd80172591039bd35a1589
Ahmad Zaidi <youremail@who.com>
2019-03-29 07:16:51 -0500

    Added unit testing

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,22 +1,62 @@
-import 'react-native';
+'use strict';
+
+import { Text } from 'react-native';
 import React from 'react';
-import App from '../App';
+import HomeScreen from '../screens/HomeScreen'
+import StatTrackerScreen from '../screens/StatTrackerScreen'
+import ItemShopScreen from '../screens/ItemShopScreen'
 import renderer from 'react-test-renderer';
-import NavigationTestUtils from 'react-navigation/NavigationTestUtils';
+import userStats from '../assets/__mockAPI'
 
-describe('App snapshot', () => {
-  jest.useFakeTimers();
-  beforeEach(() => {
-    NavigationTestUtils.resetInternalState();
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });  
+
+//Homescreen.js __Tests__
+describe('homescreen tests', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<HomeScreen />).toJSON();
+    expect(tree).toMatchSnapshot()
   });
-
-  it('renders the loading screen', async () => {
-    const tree = renderer.create(<App />).toJSON();
+  it('renders the ActivityIndicator component', () => {
+    const ActivityIndicator = require('ActivityIndicator');
+    const tree = renderer
+      .create(<ActivityIndicator animating={true} size="large" />)
+      .toJSON();
     expect(tree).toMatchSnapshot();
   });
-
-  it('renders the root without loading screen', async () => {
-    const tree = renderer.create(<App skipLoadingScreen />).toJSON();
-    expect(tree).toMatchSnapshot();
+  it('renders the Background component', done => {
+    const Image = require('Image');
+    Image.getSize('path.jpg', (width, height) => {
+      const tree = renderer.create(<Image style={{height, width}} />).toJSON();
+      expect(tree).toMatchSnapshot();
+      done();
+    });
   });
-});
+})
+
+//StatsTrackerScreen.js __Tests__
+describe('stats screen tests', () => {
+  it('renders correctly', () => {
+    const navigation = { 
+      getParam: (key, fallback) => {
+        if(key == 'username') return 'foo'
+        if(key == 'platformName') return 'foo'
+        if(key == 'userStats') return userStats
+      } 
+    }
+    const tree = renderer.create(<StatTrackerScreen navigation={navigation}/>).toJSON();
+    expect(tree).toMatchSnapshot();
+
+  });
+})
+
+//ItemShopScreen.js __Tests__
+describe('item shop screen tests', () => {
+  it('renders correctly', () => {
+    const tree = renderer.create(<ItemShopScreen />).toJSON();
+    expect(tree).toMatchSnapshot();
+
+  });
+})
---
+++ assets/__mockAPI.js
@@ -0,0 +1,264 @@
+export default userStats = 
+{
+  "defaultduo": {
+    "default": {
+      "playersoutlived": 27666,
+      "minutesplayed": 3866,
+      "lastmodified": 1553334149,
+      "matchesplayed": 3509,
+      "kills": 3841,
+      "placetop12": 971,
+      "placetop5": 451,
+      "placetop1": 87,
+      "score": 97328
+    }
+  },
+  "respawn": {
+    "24": {
+      "minutesplayed": 110,
+      "kills": 82,
+      "matchesplayed": 14,
+      "playersoutlived": 78,
+      "lastmodified": 1551505616,
+      "placetop1": 2,
+      "score": 6392
+    }
+  },
+  "trios": {
+    "default": {
+      "matchesplayed": 8,
+      "score": 2959,
+      "playersoutlived": 469,
+      "lastmodified": 1549778999,
+      "kills": 23,
+      "minutesplayed": 85
+    }
+  },
+  "snipers": {
+    "solo": {
+      "matchesplayed": 2,
+      "kills": 2
+    },
+    "duo": {
+      "placetop5": 2,
+      "placetop12": 2,
+      "kills": 4,
+      "matchesplayed": 4
+    },
+    "squad": {
+      "kills": 10,
+      "matchesplayed": 10
+    }
+  },
+  "50v50": {
+    "default": {
+      "placetop1": 6,
+      "matchesplayed": 6,
+      "kills": 8
+    }
+  },
+  "bling": {
+    "squad": {
+      "kills": 79,
+      "placetop3": 15,
+      "minutesplayed": 206,
+      "matchesplayed": 49,
+      "placetop6": 16,
+      "playersoutlived": 1546,
+      "placetop1": 12,
+      "score": 7118,
+      "lastmodified": 1552807645
+    },
+    "duo": {
+      "placetop1": 2,
+      "placetop5": 2,
+      "matchesplayed": 8,
+      "placetop12": 2,
+      "kills": 34
+    }
+  },
+  "deimos": {
+    "duo": {
+      "placetop5": 14,
+      "matchesplayed": 92,
+      "placetop12": 22,
+      "minutesplayed": 265,
+      "placetop1": 6,
+      "kills": 140,
+      "lastmodified": 1548737972,
+      "score": 6042,
+      "playersoutlived": 1839
+    },
+    "squad": {
+      "placetop6": 2,
+      "kills": 44,
+      "matchesplayed": 18
+    }
+  },
+  "close": {
+    "squad": {
+      "kills": 20,
+      "placetop6": 2,
+      "matchesplayed": 8,
+      "placetop3": 2,
+      "placetop1": 2
+    }
+  },
+  "highexplosives": {
+    "solo": {
+      "matchesplayed": 2,
+      "placetop25": 2
+    },
+    "squad": {
+      "placetop6": 2,
+      "kills": 10,
+      "matchesplayed": 10
+    }
+  },
+  "ww": {
+    "duo": {
+      "kills": 8,
+      "placetop12": 1,
+      "score": 592,
+      "minutesplayed": 27,
+      "playersoutlived": 172,
+      "lastmodified": 1549413496,
+      "placetop5": 1,
+      "matchesplayed": 7
+    }
+  },
+  "playgroundv2": {
+    "default": {
+      "score": 16760,
+      "lastmodified": 1553326622,
+      "kills": 298,
+      "minutesplayed": 1927,
+      "matchesplayed": 67
+    }
+  },
+  "defaultsquad": {
+    "default": {
+      "placetop6": 1093,
+      "minutesplayed": 1810,
+      "kills": 5037,
+      "score": 54975,
+      "placetop3": 634,
+      "lastmodified": 1553247955,
+      "placetop1": 237,
+      "playersoutlived": 11863,
+      "matchesplayed": 4290
+    }
+  },
+  "defaultsolo": {
+    "default": {
+      "score": 347,
+      "placetop1": 2,
+      "playersoutlived": 223,
+      "placetop25": 127,
+      "matchesplayed": 507,
+      "lastmodified": 1551339799,
+      "kills": 548,
+      "placetop10": 62,
+      "minutesplayed": 21
+    }
+  },
+  "solidgold": {
+    "duo": {
+      "placetop1": 1,
+      "placetop12": 19,
+      "placetop5": 10,
+      "playersoutlived": 187,
+      "kills": 71,
+      "matchesplayed": 43,
+      "score": 527,
+      "lastmodified": 1550031110,
+      "minutesplayed": 24
+    },
+    "squad": {
+      "minutesplayed": 7,
+      "playersoutlived": 51,
+      "matchesplayed": 11,
+      "kills": 7,
+      "score": 240,
+      "lastmodified": 1550897642
+    }
+  },
+  "hover": {
+    "64": {
+      "kills": 8,
+      "playersoutlived": 142,
+      "score": 2478,
+      "matchesplayed": 8,
+      "lastmodified": 1551335882,
+      "minutesplayed": 51
+    }
+  },
+  "blitz": {
+    "squad": {
+      "score": 1025,
+      "playersoutlived": 242,
+      "matchesplayed": 40,
+      "placetop3": 1,
+      "lastmodified": 1549784929,
+      "kills": 42,
+      "minutesplayed": 28,
+      "placetop6": 5
+    },
+    "duo": {
+      "matchesplayed": 28,
+      "placetop5": 2,
+      "placetop12": 10,
+      "kills": 28
+    }
+  },
+  "showdownalt": {
+    "duo": {
+      "placetop1": 8,
+      "minutesplayed": 758,
+      "matchesplayed": 113,
+      "playersoutlived": 5040,
+      "placetop12": 45,
+      "kills": 161,
+      "score": 18053,
+      "lastmodified": 1552892725,
+      "placetop5": 16
+    }
+  },
+  "5x20": {
+    "default": {
+      "matchesplayed": 6,
+      "kills": 10
+    }
+  },
+  "playground": {
+    "default": {
+      "placetop1": 14,
+      "score": 510,
+      "minutesplayed": 42,
+      "kills": 131,
+      "lastmodified": 1544772366,
+      "matchesplayed": 46
+    }
+  },
+  "creative": {
+    "playonly": {
+      "matchesplayed": 8,
+      "minutesplayed": 140,
+      "score": 1465,
+      "lastmodified": 1552982809,
+      "kills": 40
+    }
+  },
+  "soaring": {
+    "50s": {
+      "matchesplayed": 4
+    }
+  },
+  "vamp": {
+    "squad": {
+      "matchesplayed": 6,
+      "kills": 4
+    }
+  }
+  
+}
