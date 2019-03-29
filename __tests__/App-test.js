6ab0e0d9672c0ddd2b7c0812d17b92cb86d8e5a2
Ahmad Zaidi <azaidi4@wisc.edu>
2019-03-29 07:27:23 -0500

    test commit

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,6 +1,5 @@
 'use strict';
 
-import { Text } from 'react-native';
 import React from 'react';
 import HomeScreen from '../screens/HomeScreen'
 import StatTrackerScreen from '../screens/StatTrackerScreen'
