ba918b9d3c2121aa063864c7d79ed74d16374690
Connor <connorb999@gmail.com>
2019-04-08 16:52:51 -0500

    Update AppNavigator.js

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,18 +1,24 @@
 import {createStackNavigator, createAppContainer} from 'react-navigation';
 import HomeScreen from '../screens/HomeScreen';
-import ItemShopScreen from '../screens/ItemShopScreen'
+import ItemShopScreen from '../screens/ItemShopScreen';
+import PatchNotesScreen from '../screens/PatchNotesScreen';
 import RandomDropGeneratorScreen from '../screens/RandomDropGeneratorScreen';
-import StatTrackerScreen from '../screens/StatTrackerScreen'
-import WeeklyChallengesScreen from '../screens/WeeklyChallengesScreen'
-import WeekScreen from '../screens/WeekScreen'
+import StatTrackerScreen from '../screens/StatTrackerScreen';
+import UpcomingItemsScreen from '../screens/UpcomingItemsScreen';
+import WeaponStatsScreen from '../screens/WeaponStatsScreen';
+import WeeklyChallengesScreen from '../screens/WeeklyChallengesScreen';
+import WeekScreen from '../screens/WeekScreen';
 
 
 const MainNavigator = createStackNavigator(
     {
         Home: HomeScreen,
         ItemShopScreen: ItemShopScreen,
+        PatchNotesScreen: PatchNotesScreen,
         RandomDropGeneratorScreen: RandomDropGeneratorScreen,
         StatTrackerScreen: StatTrackerScreen,
+        UpcomingItemsScreen: UpcomingItemsScreen,
+        WeaponStatsScreen: WeaponStatsScreen,
         WeeklyChallengesScreen: WeeklyChallengesScreen,
         WeekScreen: WeekScreen
     },
