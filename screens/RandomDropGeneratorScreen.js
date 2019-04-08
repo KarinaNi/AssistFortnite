353b192b8acc76b354b3cc7eb5d23e434d8b2fca
Connor <connorb999@gmail.com>
2019-04-08 17:12:27 -0500

    Update RandomDropGeneratorScreen.js

--- screens/RandomDropGeneratorScreen.js
+++ screens/RandomDropGeneratorScreen.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from '../styles';
 import background from '../assets/images/background.jpg';
-import { View, Button, Text, ImageBackground, TouchableOpacity } from 'react-native';
+import { View, Text, ImageBackground, TouchableOpacity } from 'react-native';
 
 export default class RandomDropGeneratorScreen extends React.Component {
   static navigationOptions = {
@@ -9,73 +9,88 @@ export default class RandomDropGeneratorScreen extends React.Component {
   };
 
   constructor(){
-  
       super();
-  
       this.state={
-
-        PlaceHolder : ''
-  
+        PlaceHolder : '',
+        StratHolder : ''
       }
   }
-
   
-  GenerateRandomLocation=()=>
-  {
+  GenerateRandom = () => {
 
-  var RandomPlace = [
-    'Junk Junction',
-    'Haunted Hills',
-    'Sunny Steps',
-    'Lazy Lagoon',
-    'Shifty Shafts',
-    'Salty Springs',
-    'Salty Springs',
-    'Dusty Divot',
-    'Lonely Lodge',
-    'Snobby Shores',
-    'Polar Peak',
-    'Pleasant Park',
-    'Fatal Fields',
-    'Retail Row',
-    'Paradise Palms',
-    'Frosty Flights',
-    'Loot Lake',
-    'Happy Hamlet',
-    'Tilted Towers',
-    'The Block'   
-  ]
+    var RandomPlace = [
+      'Junk Junction',
+      'Haunted Hills',
+      'Sunny Steps',
+      'Lazy Lagoon',
+      'Shifty Shafts',
+      'Salty Springs',
+      'Dusty Divot',
+      'Lonely Lodge',
+      'Snobby Shores',
+      'Polar Peak',
+      'Pleasant Park',
+      'Fatal Fields',
+      'Retail Row',
+      'Paradise Palms',
+      'Frosty Flights',
+      'Loot Lake',
+      'Happy Hamlet',
+      'Tilted Towers',
+      'The Block'   
+    ]
+    var RandomStrat = [
+      'Pistols Only',
+      'No Shield',
+      'No Builds',
+      'Couch Walk',
+      'One Gun',
+      'No Heals'  
+    ]
 
-  var RandomPlace = RandomPlace[Math.floor(Math.random() * RandomPlace.length)]
-
-  this.setState({
- 
-    PlaceHolder : RandomPlace
-   
-  })
+    var RandomPlace = RandomPlace[Math.floor(Math.random() * RandomPlace.length)]
+    var RandomStrat = RandomStrat[Math.floor(Math.random() * RandomStrat.length)]
+    
+    this.setState({
+      PlaceHolder : RandomPlace,
+      StratHolder : RandomStrat
+    })
   }
 
   render() {
     return (
       <View style={styles.container}>
         <ImageBackground source={background} style={{ flex: 1 }}>
+
+        <View style={{flex:1, flexDirection:'column', justifyContent:'space-evenly'}}>
+
+          <View style={styles.homeScreenStatTrackerView}>
+            <Text style={{color:'#ffffff', fontSize: 25, fontWeight:"bold"}}>
+              {'Strategy'}
+            </Text>
+            <Text style={{color:'#ffffff', fontSize: 40, fontWeight:"bold", marginBottom: 5}}>
+              {this.state.StratHolder}
+            </Text>
+          </View>
           
           <View style={styles.homeScreenStatTrackerView}>
-            <Text style={{color:'#9d4dbb', fontSize: 25, fontWeight:"bold"}}>
+            <Text style={{color:'#ffffff', fontSize: 25, fontWeight:"bold"}}>
               {'Drop Location'}
             </Text>
-            <Text style={{color:'#9d4dbb', fontSize: 25, fontWeight:"bold", marginBottom: 5}}>
+            <Text style={{color:'#ffffff', fontSize: 40, fontWeight:"bold", marginBottom: 5}}>
               {this.state.PlaceHolder}
             </Text>
           </View>
     
           <TouchableOpacity 
-            onPress={() => this.GenerateRandomLocation()}  
+            onPress={() => this.GenerateRandom()} 
             style={styles.dropButtonStyle}>
             <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
               <Text style={styles.homeScreenButtonText}>Drop</Text>
             </View>
           </TouchableOpacity>
+
+          </View>
 
         </ImageBackground>
       </View>
