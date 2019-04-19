f8ffb22aa345f38e10dc8b061e012c7e1b13f392
Ahmad Zaidi <azaidi4@wisc.edu>
2019-04-18 19:14:33 -0500

    Added .env support

--- fire.js
+++ fire.js
@@ -1,15 +1,16 @@
+require('dotenv').config()
 const firebase = require("firebase");
 // Required for side-effects
 require("firebase/firestore");
 
 // You'll have to fill in the API credentials from your project
 const firebaseConfig = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  databaseURL: process.env.databaseURL,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DB_URL,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(firebaseConfig);
