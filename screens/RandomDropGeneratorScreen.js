7d4a6cb0286deb8720297daf3e9aadc110a062da
BoClow <boclow34@gmail.com>
2019-04-24 18:44:56 -0500

    clean drop screen

--- screens/RandomDropGeneratorScreen.js
+++ screens/RandomDropGeneratorScreen.js
@@ -1,36 +1,7 @@
 import React from 'react';
 import styles from '../styles';
 import background from '../assets/images/background.jpg';
-import { View, Text, Image, ImageBackground, TouchableOpacity } from 'react-native';
-
-import block from '../assets/images/Block.jpg'
-import castle from '../assets/images/Castle.jpg'
-import disco from '../assets/images/Disco.jpg'
-import dusty from '../assets/images/Dusty.jpg'
-import fatal from '../assets/images/Fatal.jpg'
-import frosty from '../assets/images/Frosty.jpg'
-import happy from '../assets/images/Happy.jpg'
-import haunted from '../assets/images/Haunted.jpg'
-import junk from '../assets/images/Junk.jpg'
-import lazy from '../assets/images/Lazy.jpg'
-import lonely from '../assets/images/Lonely.jpg'
-import loot from '../assets/images/Loot.jpg'
-import lucky from '../assets/images/Lucky.jpg'
-import mansion from '../assets/images/Mansion.jpg'
-import paradise from '../assets/images/Paradise.jpg'
-import polar from '../assets/images/Polar.jpg'
-import retail from '../assets/images/Retail.jpg'
-import rv from '../assets/images/RV.jpg'
-import salty from '../assets/images/Salty.jpg'
-import shifty from '../assets/images/Shifty.jpg'
-import snobby from '../assets/images/Snobby.jpg'
-import soccer from '../assets/images/Soccer.jpg'
-import spot from '../assets/images/Spot.jpg'
-import sunny from '../assets/images/Sunny.jpg'
-import tilted from '../assets/images/Tilted.jpg'
-import volcano from '../assets/images/Volcano.jpg'
-
-
+import { View, Text, ImageBackground, TouchableOpacity } from 'react-native';
 
 export default class RandomDropGeneratorScreen extends React.Component {
   static navigationOptions = {
