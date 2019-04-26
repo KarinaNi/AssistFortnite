82f505f0879a30890410155be00df8a011d486e4
BoClow <boclow34@gmail.com>
2019-04-26 17:18:18 -0500

    Update fire.js

--- fire.js
+++ fire.js
@@ -2,13 +2,15 @@ const firebase = require("firebase");
 // Required for side-effects
 require("firebase/firestore");
 
+import * as config from './FirebaseConfig'
+// You'll have to fill in the API credentials from your project
 const firebaseConfig = {
-  apiKey: "AIzaSyDgH5mSmcCw2wQOHKZSL24ZST11UqKowB0",
-    authDomain: "assistant-for-fortnite.firebaseapp.com",
-    databaseURL: "https://assistant-for-fortnite.firebaseio.com",
-    projectId: "assistant-for-fortnite",
-    storageBucket: "assistant-for-fortnite.appspot.com",
-    messagingSenderId: "1079582520882"
+  apiKey: config.API_KEY,
+  authDomain: config.AUTH_DOMAIN,
+  databaseURL: config.DB_URL,
+  projectId: config.PROJECT_ID,
+  storageBucket: config.STORAGE_BUCKET,
+  messagingSenderId: config.MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(firebaseConfig);
